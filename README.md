f0a23764843cd584fe7c80874b98456da969cb43
abdulrehman-03 <syedabdulrehman003@gmail.com>
2023-09-21 23:16:07 +0500

    first launch

---
+++ README.md
@@ -0,0 +1,38 @@
+# Games
+Fun little games I built while learning
+
+
+# Installation
+
+You can download using github gui or by running the following command in the terminal
+
+`git clone https://github.com/abdulrehman-03/Games`
+
+
+## Treasure Hunt
+
+Run the treasure_hunt.py code using python:
+
+`python3 treasure_hunt.py`
+
+Good luck finding that treasure!
+
+
+## Rock, Paper, Scissors
+
+Run the rock_paper_scissors.py code using python:
+
+`python3 rock_paper_scissors.py`
+
+Good luck against the computer!
+
+
+## Hangman
+
+Open the hangman directory using and run the hangman.py file:
+
+`cd hangman`
+
+`python3 hangman.py`
+
+Play the game and test your vocabulary!
---
+++ rock_paper_scissors.py
@@ -0,0 +1,71 @@
+# Haven't done much error handling. Just raw code
+# You are welcome to change the code and make a pull request
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+rnd = random.randint(0,2)
+
+if rnd == 0:
+  com_choice = rock
+elif rnd == 1:
+  com_choice = paper
+else:
+  com_choice = scissors
+
+user = int(input("What do you choose? 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+if user == 0:
+  user_choice = rock
+elif user == 1:
+  user_choice = paper
+elif user == 2:
+  user_choice = scissors
+else:
+  print("Please enter a valid number!")
+  
+if user == rnd:
+  result = "Draw!"
+elif user == 0 and rnd == 2:
+  result = "You won!"
+elif rnd > user:
+  result = "Computer won!"
+elif rnd == 0 and user == 2:
+  result = "Computer won!"
+else:
+  result = "You won!"
+
+
+print("You chose:")
+print(user_choice)
+
+print("Computer chose:")
+print(com_choice)
+
+print(result)
